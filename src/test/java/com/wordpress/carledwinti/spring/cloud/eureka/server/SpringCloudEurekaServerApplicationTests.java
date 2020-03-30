ecc8713fc92fa2078a9b2823fbb9f0e7ec29e2f2
carledwin <carlinstr@gmail.com>
2020-03-30 16:58:44 -0300

    Implementação inicial

---
+++ src/test/java/com/wordpress/carledwinti/spring/cloud/eureka/server/SpringCloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordpress.carledwinti.spring.cloud.eureka.server;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
